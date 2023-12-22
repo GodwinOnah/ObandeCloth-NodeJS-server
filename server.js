84a606c12b5c88bc33252beb0ab45e06e4e42b72
GodwinObande <onahgodwinobande@gmail.com>
2023-12-22 06:28:53 +0000

    Added user fetch by id, login, add user methods

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ import cors from 'cors';
 import knex from 'knex';
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 const db = knex({    //connecting to database using knex
   client: 'pg',
@@ -19,82 +20,78 @@ const db = knex({    //connecting to database using knex
 });
 
 app.get('/clothings',(req,res)=>{//Get clothings
-    db.returning('*').select('*').from('clothings')
+  return  db.select('*').from('clothings')
     .then(data=>{     
-    console.log(data);
     res.json(data);
 })
 })
 
 
 app.post('/clothings',(req,res)=>{//Add Clothings
+        const {clothName,clothPicture,clothPrice} = req.body;
+        if(!clothName||!clothPicture||!clothPrice){
+                res.status(400).json('Enter all fields');
+                return;
+        }
+                db('clothings').insert({  
+                                clopthpicture: clothPicture,                 
+                                clothname: clothName,
+                                clothprice: clothPrice                
+                            })
+                            return "Saved"
+                    })
 
-const {clothName,clothPicture,clothPrice} = req.body;
-
-if(!clothName||!clothPicture||!clothPrice){
-
-        res.status(400).json('Enter all fields')
-}
-console.log(req.body.clothPicture)
-
-db.insert({                   
-                clothname: clothName,
-                clopthpicture: clothPicture,
-                clothprice: clothPrice
-                    
-            }).then(clothings=>{
-
-        res.json(clothings[clothings.length-1]);
-
-    })
-    
-    })
-
-
-
-app.get('/userx',(req,res)=>{//Get users
-    db.select('*').from('userx')
-    .then(data=>{    
-    console.log(data);
-  return res.json(data);
-})
+    app.get('/userx',(req,res)=>{//Get users
+                return  db.select('*').from('userx')
+                .then(user=>{    
+                res.json(user);
+            })
 })
 
 app.post('/login',(req,res)=>{//login user
-    login.handleLogin(req,res,db,bcrypt)
-})
+    const {email,password} = req.body;
+        if(!email||!password){
+                res.status(400).json('Incorrect Password')
+        }
+        const hash=bcrypt.hashSync(password);//Hashing password here
+
+        db.select('password').from('userx')
+		.where({email:email})
+		.then(password=>{       
+			if(password==hash){
+              return  db.select('*').from('userx')
+                .where({email:email}).then(
+                    user=>{
+                            res.json(user[0])
+                    }
+                )				
+            }else{
+						res.status(400).json('Not found')
+				}
+		}).catch(err=>res.status(400).json('erro getting user'))
+	})
 
 app.post('/userx',(req,res)=>{//Add user
-
-const {firstName,lastName,email,phone,address,password} = req.body;
-
-if(!firstName||!lastName||!email||!phone||!address||!password){
-
-        res.status(400).json('Enter complete fields')
-}
-
-const hash=bcrypt.hashsync(req.body.password);
-
-db.insert({                   
-                    firstname: firstName,
-                    lastname: lastName,
-                    email: email,
-                    address: address,
-                    phone:phone,
-                    password:password
-
-            }).then(user=>{
-
-        res.json(user[user.length-1]);
-
-    })
-    
-    })
+        const {firstName,lastName,email,address,phone,password} = req.body;
+        if(!firstName||!lastName||!email||!phone||!address||!password){
+                res.status(400).json('Enter complete fields')
+        }
+        const hash=bcrypt.hashSync(password);//Hashing password here
+         db('userx').insert({  
+                            address: address,                                            
+                            email: email,
+                            firstname: firstName,
+                            lastname: lastName,                          
+                            password:hash,
+                            phone:phone,
+                    })
+                    .then(user=>{
+                        res.json(user[0]);
+}) })
 
 
 app.get('/userx/:id',(req,res)=>{//using Id to fetch user
-
-		db.select('*').from('userx')
+	return	db.select('*').from('userx')
 		.where({id:req.params.id})
 		.then(user=>{
 			if(user.length){
